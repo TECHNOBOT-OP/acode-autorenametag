f51c59a81ae28e4deb5c9348c182bd0571292f11
тє¢нησ вσу <116170524+TECHNOBOT-OP@users.noreply.github.com>
2024-01-12 19:15:45 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,61 @@
 import plugin from '../plugin.json';
 
-class AcodePlugin {
+class AutoRenameTagPlugin {
+  constructor() {
+    this.editor = null;
+    this.file = null;
+  }
 
-  async init() {
+  async init($page) {
+    this.editor = $page.editor;
+    this.file = $page.file;
+    this.setupEventListeners();
+  }
 
+  setupEventListeners() {
+    this.editor.onDidChangeContent((event) => this.handleContentChange(event));
   }
 
   async destroy() {
+    this.removeEventListeners();
+  }
 
+  removeEventListeners() {
+    this.editor.offDidChangeContent(this.handleContentChange);
+  }
+
+  handleContentChange(event) {
+    if (!this.isHtmlOrEjsFile()) {
+      return;
+    }
+
+    const { range, newText } = event;
+    const match = newText.match(/<(\w+)[^>]*>/);
+    if (match) {
+      const openingTag = match[0];
+      const tagName = match[1];
+      const closingTag = </${tagName}>;
+
+      this.editor.edit((editBuilder) => {
+        editBuilder.replace(range.end, closingTag);
+      });
+    }
+  }
+
+  isHtmlOrEjsFile() {
+    const fileType = this.file.type.toLowerCase();
+    return fileType === 'html' || fileType === 'ejs';
   }
 }
 
 if (window.acode) {
-  const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-    if (!baseUrl.endsWith('/')) {
-      baseUrl += '/';
-    }
-    acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init($page, cacheFile, cacheFileUrl);
+  const autoRenameTagPlugin = new AutoRenameTagPlugin();
+
+  acode.setPluginInit(plugin.id, async ($page) => {
+    await autoRenameTagPlugin.init($page);
   });
+
   acode.setPluginUnmount(plugin.id, () => {
-    acodePlugin.destroy();
+    autoRenameTagPlugin.destroy();
   });
 }
